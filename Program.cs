101ce90d5c207b6eb1112d934494a55024b586a6
HamedSY <hamed007.saboor@gmail.com>
2023-07-08 11:35:31 +0330

    Add Text.Json package & Create classes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace StudentsGrades
+{
+    public class Grade
+    {
+        public int StudentNumber;
+        public string Lesson;
+        public double Score;
+    }
+
+    public class Student
+    {
+        public int StudentNumber;
+        public string FirstName;
+        public string LastName;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
