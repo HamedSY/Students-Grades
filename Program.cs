8574d54d2304426191df56e47f3bc0d74bb75cf6
HamedSY <hamed007.saboor@gmail.com>
2023-07-08 12:01:34 +0330

    Deserialize the json files

--- Program.cs
+++ Program.cs
@@ -1,27 +1,30 @@
 ﻿using System;
+using System.Collections;
 using System.Runtime.InteropServices;
+using System.Text.Json;
 
 namespace StudentsGrades
 {
     public class Grade
     {
-        public int StudentNumber;
-        public string Lesson;
-        public double Score;
+        public int StudentNumber { get; set; }
+        public string Lesson { get; set; }
+        public double Score { get; set; }
     }
 
     public class Student
     {
-        public int StudentNumber;
-        public string FirstName;
-        public string LastName;
+        public int StudentNumber {set; get;}
+        public string FirstName {set; get;}
+        public string LastName {set; get;}
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var students = JsonSerializer.Deserialize<List<Student>>(File.ReadAllText("students.json"));
+            var grades = JsonSerializer.Deserialize<List<Grade>>(File.ReadAllText("grades.json"));
         }
     }
 }
